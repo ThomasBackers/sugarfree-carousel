24c99874c5a0fdc371148868c95947b44c130af9
ThomasBackers <thomasbackers@gmail.com>
2021-10-20 23:36:04 +0200

    on my way to simplify carousels making

--- public/js/sugarfree-carousel.js
+++ public/js/sugarfree-carousel.js
@@ -1,1 +1,36 @@
+class Carousel {
+    constructor(parentNode, options = {}) {
+        this.parentNode = parentNode
+        this.options = Object.assign({}, {
+            // slides options
+            slidesAmount: 3,
+            slidesToScroll: 1,
+            slidesVisibile: 1,
+            // carousel tag options
+            carouselTagName: 'div',
+            carouselClassName: 'carousel',
+            // panorama tag options
+            panoramaTagName: 'div',
+            panoramaClassName: 'panorama'
+        }, options)
+        this.carousel = this.setNewNode(
+            this.options.carouselTagName,
+            this.options.carouselClassName,
+            this.parentNode
+        )
+        this.panorama = this.setNewNode(
+            this.options.panoramaTagName,
+            this.options.panoramaClassName,
+            this.carousel
+        )
+    }
 
+    setNewNode(tagName, className, receiverNode) {
+        const newNode = document.createElement(`${tagName}`)
+        if (receiverNode.className) 
+            newNode.setAttribute('class', `${receiverNode.className}__${className}`)
+        else newNode.setAttribute('class', `${className}`)
+        receiverNode.appendChild(newNode)
+        return newNode
+    }
+}
